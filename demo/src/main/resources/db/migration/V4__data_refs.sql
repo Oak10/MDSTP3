8f657f4c86efb1ca3523a77a834c8aa0c8712d5c
oak10 <cl-a@live.com.pt>
2022-12-27 23:16:15 +0000

    just_missing_home

---
+++ demo/src/main/resources/db/migration/V4__data_refs.sql
@@ -0,0 +1,48 @@
+INSERT INTO refs (author, post_date, title)
+VALUES ('Treveil, M. and Omont, N. and Stenac, C. and Lefevre, K. and Phan, D. and Zentici, J. and Lavoillotte, A. and Heidmann, L. and Miyazaki, M',
+        '2020',
+        'Introducing MLOps: How to Scale Machine Learning in the Enterprise'
+);
+
+INSERT INTO refs (author, post_date, title)
+VALUES ('Subramanya R, Sierla S, Vyatkin V.',
+        '2022',
+        'From DevOps to MLOps: Overview and Application to Electricity Market Forecastin'
+);
+
+INSERT INTO refs (author, post_date, title)
+VALUES ('M. M. John, H. H. Olsson and J. Bosch',
+        '2021',
+        'Towards MLOps: A Framework and Maturity Model'
+);
+
+INSERT INTO refs (author, post_date, title)
+VALUES ('Renggli, Cedric and Rimanic, Luka and Gürel, Nezihe Merve and Karlaš, Bojan and Wu, Wentao and Zhang',
+        '2021',
+        'A Data Quality-Driven View of MLOps'
+);
+
+INSERT INTO refs (author, post_date, title)
+VALUES ('Kreuzberger, Dominik and Kühl, Niklas and Hirschl, Sebastian',
+        '2022',
+        'Machine Learning Operations (MLOps): Overview, Definition, and Architecture'
+);
+
+INSERT INTO refs (author, post_date, title)
+VALUES ('S. Mäkinen, H. Skogström, E. Laaksonen and T. Mikkonen',
+        '2021',
+        'Who Needs MLOps: What Data Scientists Seek to Accomplish and How Can MLOps Help?'
+);
+
+INSERT INTO refs (author, post_date, title)
+VALUES ('Ruf P, Madan M, Reich C, Ould-Abdeslam D.',
+        '2021',
+        'Demystifying MLOps and Presenting a Recipe for the Selection of Open-Source Tools'
+);
+
+INSERT INTO refs (author, post_date, title)
+VALUES ('G. Symeonidis, E. Nerantzis, A. Kazakis and G. A. Papakostas',
+        '2022',
+        'MLOps - Definitions, Tools and Challenges'
+);
+
